cf6a24dcb2bdf5e5e68f6f34bab9e18a07bb62a7
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-03-31 12:54:52 +0000

    update vlan assignment

--- office.tf
+++ office.tf
@@ -21,12 +21,12 @@ provider "junos-ex-interfaces" {
 
 module "iface_ge0" {
   // interface ge-0/0/0 description "tf: Raspberry Pi"
-  // interface ge-0/0/0 unit 0 family ethernet switching interface-mode access vlan members VLAN_104"
+  // interface ge-0/0/0 unit 0 family ethernet switching interface-mode access vlan members VLAN_103"
   interface_name        = "ge-0/0/0"
   subinterface_unit     = "0"
   interface_description = "tf: Raspberry Pi"
   interface_mode        = "access"
-  interface_vlans       = "VLAN_104"
+  interface_vlans       = "VLAN_103"
 
   // name of our configuration apply groups
   interface_mode_group        = "ge0_mode"
@@ -139,6 +139,26 @@ module "iface_ge5" {
   depends_on = [junos-ex-interfaces_destroycommit.commit-main]
 }
 
+module "iface_ge11" {
+  // interface ge-0/0/11 description "tf: uplink to closet"
+  // interface ge-0/0/11 unit 0 family ethernet switching interface-mode trunk vlan members all"
+  interface_name        = "ge-0/0/11"
+  subinterface_unit     = "0"
+  interface_description = "tf: Raspberuplink to closet"
+  interface_mode        = "trunk"
+  interface_vlans       = "all"
+
+  // name of our configuration apply groups
+  interface_mode_group        = "ge11_mode"
+  interface_description_group = "ge11_description"
+  interface_vlans_group       = "ge11_vlans"
+
+  // passing information into our provider
+  source     = "./interfaces"
+  providers  = { junos-ex-interfaces = junos-ex-interfaces }
+  depends_on = [junos-ex-interfaces_destroycommit.commit-main]
+}
+
 provider "junos-ex-vlans" {
   host     = "office"
   port     = var.juniper_ssh_port
@@ -206,7 +226,7 @@ module "vlan_104" {
 module "vlan_105" {
   // vlans VLAN_105 vlan-id 105 l3-interface irb.105 description "tf: management VLAN"
   vlan_name              = "VLAN_105"
-  vlan_description       = "tf: management VLAN"
+  vlan_description       = "tf: network management VLAN"
   vlan_description_group = "vlan105_description"
   vlan_id                = "105"
   vlan_id_group          = "vlan105_id"
@@ -221,7 +241,15 @@ module "vlan_105" {
 
 resource "junos-ex-interfaces_commit" "commit-main" {
   resource_name = "commit"
-  depends_on    = [module.iface_ge0, module.iface_ge1, module.iface_ge2, module.iface_ge3, module.iface_ge4, module.iface_ge5]
+  depends_on = [
+    module.iface_ge0,
+    module.iface_ge1,
+    module.iface_ge2,
+    module.iface_ge3,
+    module.iface_ge4,
+    module.iface_ge5,
+    module.iface_ge11
+  ]
 }
 
 resource "junos-ex-interfaces_destroycommit" "commit-main" {
@@ -230,7 +258,13 @@ resource "junos-ex-interfaces_destroycommit" "commit-main" {
 
 resource "junos-ex-vlans_commit" "commit-main" {
   resource_name = "commit"
-  depends_on    = [module.vlan_101, module.vlan_102, module.vlan_103, module.vlan_104, module.vlan_105]
+  depends_on = [
+    module.vlan_101,
+    module.vlan_102,
+    module.vlan_103,
+    module.vlan_104,
+    module.vlan_105
+  ]
 }
 
 resource "junos-ex-vlans_destroycommit" "commit-main" {
